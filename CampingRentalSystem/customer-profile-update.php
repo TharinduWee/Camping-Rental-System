9daa5a068736d400f8735f9a05936dddaa94388c
TharinduWee <101544414+TharinduWee@users.noreply.github.com>
2023-07-30 19:05:50 +0530

    changes done

--- CampingRentalSystem/customer-profile-update.php
+++ CampingRentalSystem/customer-profile-update.php
@@ -27,6 +27,11 @@ if (isset($_POST['form1'])) {
         $error_message .= LANG_VALUE_123."<br>";
     }
 
+    if(empty($_POST['cust_NIC'])) {
+        $valid = 0;
+        $error_message .= LANG_VALUE_164."<br>";
+    }
+
     if(empty($_POST['cust_phone'])) {
         $valid = 0;
         $error_message .= LANG_VALUE_124."<br>";
@@ -37,52 +42,19 @@ if (isset($_POST['form1'])) {
         $error_message .= LANG_VALUE_125."<br>";
     }
 
-    if(empty($_POST['cust_country'])) {
-        $valid = 0;
-        $error_message .= LANG_VALUE_126."<br>";
-    }
-
-    if(empty($_POST['cust_city'])) {
-        $valid = 0;
-        $error_message .= LANG_VALUE_127."<br>";
-    }
-
-    if(empty($_POST['cust_state'])) {
-        $valid = 0;
-        $error_message .= LANG_VALUE_128."<br>";
-    }
-
-    if(empty($_POST['cust_zip'])) {
-        $valid = 0;
-        $error_message .= LANG_VALUE_129."<br>";
-    }
 
     if($valid == 1) {
-
+        $success_message = "Your profile details updated successfully.";
         // update data into the database
-        $statement = $pdo->prepare("UPDATE tbl_customer SET cust_name=?, cust_cname=?, cust_phone=?, cust_country=?, cust_address=?, cust_city=?, cust_state=?, cust_zip=? WHERE cust_id=?");
+        $statement = $pdo->prepare("UPDATE tbl_customer SET cust_name=?, cust_NIC=?, cust_phone=?, cust_address=? WHERE cust_id=?");
         $statement->execute(array(
-                    strip_tags($_POST['cust_name']),
-                    strip_tags($_POST['cust_cname']),
-                    strip_tags($_POST['cust_phone']),
-                    strip_tags($_POST['cust_country']),
-                    strip_tags($_POST['cust_address']),
-                    strip_tags($_POST['cust_city']),
-                    strip_tags($_POST['cust_state']),
-                    strip_tags($_POST['cust_zip']),
-                    $_SESSION['customer']['cust_id']
-                ));  
-       
-        $success_message = LANG_VALUE_130;
-
-        $_SESSION['customer']['cust_name'] = $_POST['cust_name'];
-        $_SESSION['customer']['cust_cname'] = $_POST['cust_cname'];
-        $_SESSION['customer']['cust_phone'] = $_POST['cust_phone'];
-        $_SESSION['customer']['cust_country'] = $_POST['cust_country'];
-        $_SESSION['customer']['cust_address'] = $_POST['cust_address'];
-        $_SESSION['customer']['cust_city'] = $_POST['cust_city'];
-        $_SESSION['customer']['cust_state'] = $_POST['cust_state'];
-        $_SESSION['customer']['cust_zip'] = $_POST['cust_zip'];
+            strip_tags($_POST['cust_name']),
+            strip_tags($_POST['cust_NIC']),
+            strip_tags($_POST['cust_phone']),
+            strip_tags($_POST['cust_address']),
+            $_SESSION['customer']['cust_id']
+        ));
+        
     }
 }
 ?>
@@ -114,8 +86,8 @@ if (isset($_POST['form1'])) {
                                 <input type="text" class="form-control" name="cust_name" value="<?php echo $_SESSION['customer']['cust_name']; ?>">
                             </div>
                             <div class="col-md-6 form-group">
-                                <label for=""><?php echo LANG_VALUE_103; ?></label>
-                                <input type="text" class="form-control" name="cust_cname" value="<?php echo $_SESSION['customer']['cust_cname']; ?>">
+                                <label for=""><?php echo LANG_VALUE_165; ?></label>
+                                <input type="text" class="form-control" name="cust_NIC" value="<?php echo $_SESSION['customer']['cust_NIC']; ?>">
                             </div>
                             <div class="col-md-6 form-group">
                                 <label for=""><?php echo LANG_VALUE_94; ?> *</label>
@@ -129,34 +101,7 @@ if (isset($_POST['form1'])) {
                                 <label for=""><?php echo LANG_VALUE_105; ?> *</label>
                                 <textarea name="cust_address" class="form-control" cols="30" rows="10" style="height:70px;"><?php echo $_SESSION['customer']['cust_address']; ?></textarea>
                             </div>
-                            <div class="col-md-6 form-group">
-                                <label for=""><?php echo LANG_VALUE_106; ?> *</label>
-                                <select name="cust_country" class="form-control">
-                                <?php
-                                $statement = $pdo->prepare("SELECT * FROM tbl_country ORDER BY country_name ASC");
-                                $statement->execute();
-                                $result = $statement->fetchAll(PDO::FETCH_ASSOC);
-                                foreach ($result as $row) {
-                                    ?>
-                                    <option value="<?php echo $row['country_id']; ?>" <?php if($row['country_id'] == $_SESSION['customer']['cust_country']) {echo 'selected';} ?>><?php echo $row['country_name']; ?></option>
-                                    <?php
-                                }
-                                ?>
-                                </select>                                    
-                            </div>
                             
-                            <div class="col-md-6 form-group">
-                                <label for=""><?php echo LANG_VALUE_107; ?> *</label>
-                                <input type="text" class="form-control" name="cust_city" value="<?php echo $_SESSION['customer']['cust_city']; ?>">
-                            </div>
-                            <div class="col-md-6 form-group">
-                                <label for=""><?php echo LANG_VALUE_108; ?> *</label>
-                                <input type="text" class="form-control" name="cust_state" value="<?php echo $_SESSION['customer']['cust_state']; ?>">
-                            </div>
-                            <div class="col-md-6 form-group">
-                                <label for=""><?php echo LANG_VALUE_109; ?> *</label>
-                                <input type="text" class="form-control" name="cust_zip" value="<?php echo $_SESSION['customer']['cust_zip']; ?>">
-                            </div>
                         </div>
                         <input type="submit" class="btn btn-primary" value="<?php echo LANG_VALUE_5; ?>" name="form1">
                     </form>
